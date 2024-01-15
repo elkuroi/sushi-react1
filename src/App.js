59dc41d895a667448e9bd10a5084d39d79d403e8
Yehor <115032078+elkuroi@users.noreply.github.com>
2024-01-15 13:03:34 +0100

    --

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './img/logo.jpg';
+import './App.css';
+import Navbar from './Components/Navbar'
+
+function App() {
+  return (
+    <div className="App">
+      <body>
+      <img className='logo' src={logo} alt="image"/>
+      </body>
+    </div>
+  );
+}
+
+export default App;
