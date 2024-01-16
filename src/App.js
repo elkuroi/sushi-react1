4643365d8f8a080bf23a1562d1cca480b376670d
Yehor <115032078+elkuroi@users.noreply.github.com>
2024-01-16 18:41:43 +0100

    comitt

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
 import Navbar from './Components/Navbar/Navbar'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Cart from './Pages/Cart';
+
 
 function App() {
   return (
     <div className="App">
-      <body>
+      
+        <BrowserRouter>
       <Navbar/>
-      </body>
+        <Routes>
+          <Route path='/cart' element={<Cart/>}/>
+          <Route path='/login' element={<LoginSignup/>}/>
+          
+        </Routes>
+      </BrowserRouter>
+      
     </div>
   );
 }
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,24 +1,29 @@
-import React from "react";
-import './Navbar.css'
+import React, { useState } from "react";
+import './Navbar.css';
 import logo from '../img/logo.png';
 import cart_icon from '../img/cart_icon.png';
+import Link from "react";
 
 const Navbar = () =>{
+
+    const [menu,setMenu] = useState("shop");
+
     return(
         <div className="navbar">
             <div className="logo_p"> 
                 <img className='logo' src={logo} alt="image"/>
-                <p>SUSHI-SHOP</p>
+                <p>SUSHI SHOP</p>
             </div>
             
             <ul className="nav-menu">
-                <li>Menu 1</li>
-                <li>Menu 2</li>
-                <li>Menu 3</li>
+                <li onClick={()=>{setMenu("main")}}> <Link to='/'>MAIN PAGE</Link>  {menu==="main"?<hr/>:<></>}</li>
+                <li onClick={()=>{setMenu("menu")}}> <Link to='/menu'>SUSHI MENU</Link>   {menu==="menu"?<hr/>:<></>}</li>
+                <li onClick={()=>{setMenu("about")}}><Link to='/about'>ABOUT US</Link>  {menu==="about"?<hr/>:<></>}</li>
             </ul>
             <div className="nav-login-cart">
-                <button>Login</button>
-            <img className="cart_icon" src={cart_icon} alt="image" />
+                <Link to='/login'><button>Login</button></Link> 
+                <Link to='/cart'> <img className="cart_icon" src={cart_icon} alt="image"/> </Link>
+            <div className="nav-cart-count">0</div>
             </div>
         </div>
     )
