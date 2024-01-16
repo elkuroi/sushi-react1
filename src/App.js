ad5091d62cf9d378feba1ef7169a68cded671c30
Yehor <115032078+elkuroi@users.noreply.github.com>
2024-01-16 18:42:10 +0100

    aqaa

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ function App() {
         <Routes>
           <Route path='/cart' element={<Cart/>}/>
           <Route path='/login' element={<LoginSignup/>}/>
-          
         </Routes>
       </BrowserRouter>
       
